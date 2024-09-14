4df08ec3b1b17f9842fbf16c55bad6cdaa4f471a
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-14 21:46:54 +0700

    feat: add login and register form component

---
+++ src/features/auth/components/form/RegisterForm.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { Card, Input, Button, Typography } from "@material-tailwind/react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { register } from "../../slice/authSlice";
+
+const RegisterForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await dispatch(
+        register({ name, email, password, roles: "staff" })
+      );
+      console.log({ res });
+      if (res?.payload?.success) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
+  return (
+    <Card color="transparent" shadow={false}>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-1 flex flex-col gap-6">
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Name
+          </Typography>
+          <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Email
+          </Typography>
+          <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Password
+          </Typography>
+          <Input
+            type="password"
+            size="lg"
+            placeholder="********"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <Button className="mt-6" fullWidth onClick={(e) => handleSubmit(e)}>
+          Register
+        </Button>
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          Already have an account?{" "}
+          <Link to="/login" className="font-medium text-gray-900">
+            Login
+          </Link>
+        </Typography>
+      </form>
+    </Card>
+  );
+};
+
+export default RegisterForm;
