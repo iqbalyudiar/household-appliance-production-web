35fe3858cd4a256458bbd1451385165d6cf4929d
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-14 21:43:38 +0700

    feat: add register page to router utils

--- src/utils/routesConfig.ts
+++ src/utils/routesConfig.ts
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import LoginPage from "../pages/auth/LoginPage.tsx";
+import RegisterPage from "../pages/auth/RegisterPage.tsx";
 import ProductPage from "../pages/products/ProductPage";
 import WarrantyPage from "../pages/warranty/WarrantyPage";
 import HomePage from "../pages/Home/HomePage.tsx";
@@ -15,6 +16,7 @@ interface RouteConfig {
 const routes: RouteConfig[] = [
     { path: '/', component: HomePage },
     { path: '/login', component: LoginPage },
+    { path: '/register', component: RegisterPage },
     { path: '/products', component: ProductPage },
     { path: '/warranty', component: WarrantyPage },
     { path: '*', component: NotFoundPage }
