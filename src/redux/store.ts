0546787a10b75d71a3170b19863cebadc20ef603
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-14 21:44:37 +0700

    feat: add new reducer auth

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authSlice from '../features/auth/slice/authSlice';
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        auth: authSlice
+    }
 })
 
 export default store;
