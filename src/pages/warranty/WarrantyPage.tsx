ac16080bcd20805c1adf0850d5d6d051240e5923
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-17 20:33:44 +0700

    feat: add warranty header and modal to warranty page

--- src/pages/warranty/WarrantyPage.tsx
+++ src/pages/warranty/WarrantyPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import WarrantyList from "../../features/warranty/components/list/WarrantyList";
-
+import WarrantyHeader from "../../features/warranty/components/header/WarrantyHeader";
+import WarrantyModal from "../../features/warranty/components/modal/WarrantyModal";
 const WarrantyPage: React.FC = () => {
   return (
     <div>
+      <WarrantyHeader />
       <WarrantyList />
+      <WarrantyModal />
     </div>
   );
 };
