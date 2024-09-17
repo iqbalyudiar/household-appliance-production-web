43c3dc6155db6f43cb0dc5fff7d0c4c354ee7e81
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-17 08:31:41 +0700

    feat: change styling auth layout

--- src/features/auth/components/layout/AuthLayout.tsx
+++ src/features/auth/components/layout/AuthLayout.tsx
@@ -13,8 +13,8 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({
   description,
 }) => {
   return (
-    <div className="flex items-center justify-center min-h-screeng-gray-100">
-      <div className="w-full p-8 bg-white shadow-lg rounded-lg">
+    <div className="flex items-center justify-center min-h-screen-gray-100 h-screen w-screen">
+      <div className="flex flex-col items-center w-3/4 md:w-1/2 p-8 bg-white shadow-lg rounded-lg">
         <h1 className="text-2xl font-bold mb-6">{title}</h1>
         <Typography color="gray" className="mt-1 font-normal">
           {description}
