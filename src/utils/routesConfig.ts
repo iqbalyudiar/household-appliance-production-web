9af72e14cac50ec848a988bc5e1912c7767865c5
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-17 22:32:17 +0700

    feat: add name to routes config

--- src/utils/routesConfig.ts
+++ src/utils/routesConfig.ts
@@ -6,20 +6,27 @@ import WarrantyPage from "../pages/warranty/WarrantyPage";
 import HomePage from "../pages/Home/HomePage.tsx";
 import NotFoundPage from "../pages/notfound/NotFoundPage";
 
+import {
+    HomeIcon
+  } from "@heroicons/react/24/solid";
+
+
 interface RouteConfig {
-    path: string;
-    element?: ReactNode;
-    component?: React.FC;
-    layout?: ReactNode;
+  name: string;
+  path: string;
+  element?: ReactNode;
+  component?: React.FC;
+  layout?: ReactNode;
+  icon?: any;
 }
 
 const routes: RouteConfig[] = [
-    { path: '/', component: HomePage },
-    { path: '/login', component: LoginPage },
-    { path: '/register', component: RegisterPage },
-    { path: '/products', component: ProductPage },
-    { path: '/warranty', component: WarrantyPage },
-    { path: '*', component: NotFoundPage }
-]
+  { name: "Home", path: "", component: HomePage, icon: HomeIcon },
+  { name: "Login", path: "login", component: LoginPage },
+  { name: "Register", path: "register", component: RegisterPage },
+  { name: "Product", path: "products", component: ProductPage },
+  { name: "Warranty", path: "warranty", component: WarrantyPage },
+  { name: "Not Found", path: "*", component: NotFoundPage },
+];
 
-export default routes
+export default routes;
