5024264a79efacbaca8b4d4d8a067af89d5e670a
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-17 20:31:38 +0700

    feat: add warranty api for create and update claim

--- src/features/warranty/api/warrantyApi.ts
+++ src/features/warranty/api/warrantyApi.ts
@@ -1,6 +1,22 @@
 import api from "../../../utils/api";
+import { IClaimStatus } from "../slice/warrantySlice";
 
 export const getWarrantiesListApi = async () => {
   const response = await api.get("/warranty/claims/all");
+  return response.data;
+};
+
+export const createWarrantyClaim = async (productId: string) => {
+  const response = await api.post("/warranty/claim", { productId });
+  return response.data;
+};
+
+export const updateWarrantyClaimStatus = async ({
+  claimId,
+  status,
+}: IClaimStatus) => {
+  const response = await api.post(`/warranty/claims/${claimId}/status`, {
+    status,
+  });
   return response.data;
 };
